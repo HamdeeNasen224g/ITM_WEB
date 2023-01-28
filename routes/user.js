b3292d7ec5a3767a73ef69c1f265fc5cae0ee1d7
Praweenporn <71056576+Praweenporn@users.noreply.github.com>
2023-01-28 19:15:55 +0700

    add layout

---
+++ routes/user.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const UserController = require("../controllers/UsersController")
+const router = require('express').Router();
+
+// router.get('/',(req, res, next) => {res.send("Home");});
+router.get('/', (req, res,next) => {
+    res.send('index')
+  })
