91bfecb985022f2498a1c1f4e5669aee7a0c2ca0
Hamdee <75886000+HamdeeNasen224g@users.noreply.github.com>
2023-01-29 20:50:26 +0700

    validdate

--- routes/router.js
+++ routes/router.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const UserController = require("../controllers/UsersController")
 const router = require('express').Router();
-co 
+
 router.get('/alluser',UserController.geralluser)
 router.post('/adduser',UserController.addnewusers)
 router.post('/deleteuser',UserController.deleteuser)
