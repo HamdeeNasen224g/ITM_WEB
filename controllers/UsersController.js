31d42ca526c316883c40951f013696c2b9e1d01c
Hamdee <75886000+HamdeeNasen224g@users.noreply.github.com>
2023-01-29 21:12:51 +0700

    validate

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,5 +1,6 @@
 const Usermodel = require('../models/User')
 const user = require('../models/User')
+const {validationResult} = require("express-validator")
 class UserController{
 
     static async geralluser(req,res){
@@ -21,13 +22,18 @@ class UserController{
     }
     static async deleteuser(req,res){
         const id = req.body.id;
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            res.json(errors.array())
+        }else{     
         if(id){
             var results = await Usermodel.deleteuser(id)
             if(results)
              res.send("delete done");
             else
              res.send("delete failed");
-        }    
+        }  
+    }  
     }
     static async updateuser(req,res){
 
--- routes/router.js
+++ routes/router.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const UserController = require("../controllers/UsersController")
 const router = require('express').Router();
+const {check} = require("express-validator");
 
 router.get('/alluser',UserController.geralluser)
 router.post('/adduser',UserController.addnewusers)
-router.post('/deleteuser',UserController.deleteuser)
+router.post('/deleteuser',[check("id").exists().withMessage("id is required").isNumeric().withMessage("id should be only number")],UserController.deleteuser)
 router.post('/edituser',UserController.updateuser)
 
 module.exports = router
